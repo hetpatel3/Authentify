d11f183807558acd37eb42c924d5e243c27df35c
hetpatel3 <hetr03@gmail.com>
2025-06-04 15:54:13 +0530

    fix database connectivity issue

--- src/database/auth.database.js
+++ src/database/auth.database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDb = async() => {
     try {
-        const connectMe = await mongoose.connect(`${process.env.MONGODB_URI}/authProject`)
+        const connectMe = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log("Connected Successfully!")
     } catch (error) {
         console.log("MongoDB Connection Failed!", error);
